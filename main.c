d38e881b41945e242704606d3a72253b5e8f8834
ds-costa <davidshcosta@gmail.com>
2021-10-08 15:01:46 -0300

    hello world script file

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello world!\n");
+
+    return 0;
+}
