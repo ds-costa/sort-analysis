09f517c65c2489559b5a57bdb3742a2b218b2362
ds-costa <davidshcosta@gmail.com>
2021-10-08 15:37:30 -0300

    feat: sort functions interface

---
+++ src/sort.h
@@ -0,0 +1,59 @@
+/****************************************
+*	TITLE: Sort functions
+*	AUTHOR: David S. Costa		
+*****************************************/
+
+#ifndef SORT_H
+#define SORT_H
+
+/**
+ * function: insertion_sort
+ * Insertion sort implementation
+ * @param arr_len, Array length
+ * @param array, Integer array reference
+ */
+void insertion_sort( int arr_len, int *arr );
+
+/**
+ * function: insertion_sort
+ * Insertion sort implementation
+ * @param arr_len, Array length
+ * @param array, Integer array reference
+ */
+void shell_sort( int arr_len, int *arr );
+
+/**
+ * function: insertion_sort
+ * Insertion sort implementation
+ * @param arr_len, Array length
+ * @param array, Integer array reference
+ */
+void merge_sort( int arr_len, int *arr );
+
+/**
+ * function: quick_sort
+ * Quick sort implementation
+ * @param low, Array inferior bound. On function call: (low = 0) 
+ * @param high, Array superior bound
+ * @param array, Integer array reference
+ */
+void quick_sort(int low, int high, int *arr);
+
+/**
+ * function: bucket_sort
+ * Bucket sort implementation
+ * @param arr_len, Array length
+ * @param array, Integer array reference
+ */
+void bucket_sort( int arr_len, int *arr );
+
+/**
+ * function: quick_sort
+ * Counting sort implementation
+ * @param arr_len, Array length
+ * @param array, Integer array reference
+ */
+void counting_sort( int arr_len, int *arr );
+
+
+#endif
