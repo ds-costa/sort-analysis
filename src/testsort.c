fb02dc38e5824f995bf3ac9af467d69b668fddf5
ds-costa <davidshcosta@gmail.com>
2021-10-09 13:08:44 -0300

    testsort update, add array copy function

--- src/testsort.c
+++ src/testsort.c
@@ -1,6 +1,7 @@
 #include "testsort.h"
 
 static const char* test_insertion_sort(int arr_len, int *arr) {
+    insertion_sort(arr_len, arr);
     if(arr_is_array_sorted(arr_len, arr) == true) {
         return "TEST CASE 1[INSERTION_SORT]: SUCESS";
     }
@@ -8,6 +9,7 @@ static const char* test_insertion_sort(int arr_len, int *arr) {
 }
 
 static const char* test_shell_sort(int arr_len, int *arr) {
+    shell_sort(arr_len, arr);
     if(arr_is_array_sorted(arr_len, arr) == true) {
         return "TEST CASE 1[SHELL_SORT]: SUCESS";
     }
@@ -15,6 +17,7 @@ static const char* test_shell_sort(int arr_len, int *arr) {
 }
 
 static const char* test_merge_sort(int arr_len, int *arr) {
+    merge_sort(arr_len, arr);
     if(arr_is_array_sorted(arr_len, arr) == true) {
         return "TEST CASE 1[MERGE_SORT]: SUCESS";
     }
@@ -22,6 +25,7 @@ static const char* test_merge_sort(int arr_len, int *arr) {
 }
 
 static const char* test_quick_sort(int arr_len, int *arr) {
+    quick_sort(0, arr_len, arr);
     if(arr_is_array_sorted(arr_len, arr) == true) {
         return "TEST CASE 1[QUICK_SORT]: SUCESS";
     }
@@ -29,6 +33,7 @@ static const char* test_quick_sort(int arr_len, int *arr) {
 }
 
 static const char* test_bucket_sort(int arr_len, int *arr) {
+    bucket_sort(arr_len, arr);
     if(arr_is_array_sorted(arr_len, arr) == true) {
         return "TEST CASE 1[BUCKET_SORT]: SUCESS";
     }
@@ -36,6 +41,7 @@ static const char* test_bucket_sort(int arr_len, int *arr) {
 }
 
 static const char* test_counting_sort(int arr_len, int *arr) {
+    counting_sort(arr_len, arr);
     if(arr_is_array_sorted(arr_len, arr) == true) {
         return "TEST CASE 1[COUNTING_SORT]: SUCESS";
     }
@@ -46,17 +52,29 @@ void run_sort_tests() {
 
 
     int arr_len = 1000;
+    int test_case_1_data[arr_len];
     int test_case_1[arr_len];
     int test_case_2[arr_len];
     int test_case_3[arr_len];
 
-    arr_random_fill_int(0, 10000, arr_len, test_case_1);
+    arr_random_fill_int(0, 10000, arr_len, test_case_1_data);
 
+    arr_copy(arr_len, test_case_1_data, test_case_1);
     printf("%s\n", test_insertion_sort(arr_len, test_case_1));
+
+    arr_copy(arr_len, test_case_1_data, test_case_1);
     printf("%s\n", test_shell_sort(arr_len, test_case_1));
+
+    arr_copy(arr_len, test_case_1_data, test_case_1);
     printf("%s\n", test_merge_sort(arr_len, test_case_1));
+
+    arr_copy(arr_len, test_case_1_data, test_case_1);
     printf("%s\n", test_quick_sort(arr_len, test_case_1));
+
+    arr_copy(arr_len, test_case_1_data, test_case_1);
     printf("%s\n", test_bucket_sort(arr_len, test_case_1));
+
+    arr_copy(arr_len, test_case_1_data, test_case_1);
     printf("%s\n", test_counting_sort(arr_len, test_case_1));
 
     return;
