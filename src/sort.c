1f4d2e63699f0b3defda60c6991fe9a9a9f20dc2
ds-costa <davidshcosta@gmail.com>
2021-10-09 13:18:59 -0300

    feat(sort): insertion sort

--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,16 @@
  * @param arr_len, Array length
  * @param array, Integer array reference
  */
-void insertion_sort( int arr_len, int *arr ) {
+void insertion_sort(int arr_len, int *arr) {
+    for(int i = 1; i < arr_len; i++) {
+        int aux = arr[i];
+        int j = i - 1;
+        while(j >= 0 && arr[j] > aux) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = aux;
+    }
     return;
 }
 
