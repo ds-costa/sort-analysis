4462b1c8723006f21168af589ab4531ea2c0805d
ds-costa <davidshcosta@gmail.com>
2021-10-13 17:38:35 -0300

    feat(main): app argument handling

--- main.c
+++ main.c
@@ -6,28 +6,84 @@
 #include <unistd.h>
 
 #include "src/array.h"
+#include "src/linkedlist.h"
+#include "src/timer.h"
+#include "src/sort.h"
 
 int main(int argc, char **argv) {
 
-    printf("Hello world!\n");
+    if(argc < 4) {
+        return -1;
+    }
+    // ./bin/app  insertion r 1024
+    srand(time(NULL));
+    
+   Timer timer = {
+        .start = {0, 0},
+        .end = {0, 0},
+        .start_ns = 0,
+        .end_ns = 0,
+        .elapsed_time = 0
+    }; 
 
-    int arr[5];
-    int arr2[5];
+    int arr_len = atoi(argv[3]);
+    int *arr = malloc(sizeof(int) * arr_len);
 
-    arr_scan_int(5, arr);
-    arr_print_int(5, arr);
-    arr_copy(5, arr, arr2);
-    arr_print_int(5, arr2);
+    switch((char) argv[2][0]) {
+        case 'r':
+            arr_random_fill_int(0, 100000, arr_len, arr);
+            break;
 
-    // arr_fill_in_ascending_order(5, arr);
-    // arr_print_int(5, arr);
-    // printf("sorted: %d\n", arr_is_array_sorted(5, arr));
+        case 'a':
+            arr_fill_in_ascending_order(arr_len, arr);
+            break;
 
-    // arr_fill_in_descending_order(5, arr);
-    // arr_print_int(5, arr);
-    // printf("sorted: %d\n", arr_is_array_sorted(5, arr));
+        case 'd':
+            arr_fill_in_descending_order(arr_len, arr);
+            break;
 
+        default:
+            arr_random_fill_int(0, 100000, arr_len, arr);
+            break;
+    }
 
+    if(strcmp("insertion", argv[1]) == 0) {
+        clock_gettime(CLOCK_MONOTONIC, &timer.start);
+        insertion_sort(arr_len, arr);
+        clock_gettime(CLOCK_MONOTONIC, &timer.end);
+    }
+    else if(strcmp("shell", argv[1]) == 0) {
+        clock_gettime(CLOCK_MONOTONIC, &timer.start);
+        shell_sort(arr_len, arr);
+        clock_gettime(CLOCK_MONOTONIC, &timer.end);
+    }
+    else if(strcmp("merge", argv[1]) == 0) {
+        clock_gettime(CLOCK_MONOTONIC, &timer.start);
+        merge_sort(arr_len, arr);
+        clock_gettime(CLOCK_MONOTONIC, &timer.end);
+    }
+    else if(strcmp("quick", argv[1]) == 0) {
+        clock_gettime(CLOCK_MONOTONIC, &timer.start);
+        quick_sort(arr_len, arr);
+        clock_gettime(CLOCK_MONOTONIC, &timer.end);
+    }
+    else if(strcmp("bucket", argv[1]) == 0) {
+        clock_gettime(CLOCK_MONOTONIC, &timer.start);
+        bucket_sort(arr_len, arr);
+        clock_gettime(CLOCK_MONOTONIC, &timer.end);
+    }
+    else if(strcmp("counting", argv[1]) == 0) {
+        clock_gettime(CLOCK_MONOTONIC, &timer.start);
+        counting_sort(arr_len, arr);
+        clock_gettime(CLOCK_MONOTONIC, &timer.end);
+    }
+
+    // Calcula o tempo final
+    timer.start_ns = tm_timespec_to_nanosconds(timer.start);
+    timer.end_ns = tm_timespec_to_nanosconds(timer.end);
+    timer.elapsed_time = timer.end_ns - timer.start_ns;
+    fflush(stdout);
+    fprintf(stdout, "%ld\n", timer.elapsed_time);
 
     return 0;
 }
