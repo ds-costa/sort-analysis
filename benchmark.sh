604e689c2c31ec5711d644df71e168c65bc3e260
ds-costa <davidshcosta@gmail.com>
2021-10-19 12:02:42 -0300

    benchmark script v2

--- benchmark.sh
+++ benchmark.sh
@@ -5,10 +5,11 @@ time_mean=0
 
 number_of_sorts=6 
 number_of_inputs=2
-number_of_cases=3
+number_of_cases=10
 number_of_test=$(bc <<< "$number_of_sorts * $number_of_cases * $number_of_inputs * 10")
 actual_test=1
 rm -rf benchmarks
+rm temp.arr
 mkdir benchmarks
 
 # Argumentos: sort caso entrada, cada sort testa todos os casos e entradas
@@ -23,7 +24,7 @@ do
     do
         printf "input(n) time(ns)\n" $n $elapsed_time >> benchmarks/$sort/$_case.txt  
 
-        for n in 1024 2048
+        for n in 1000 5000 10000 25000 50000 75000 100000 250000 500000 1000000
         do
             # Para cada caso 10 execuções
             touch benchmarks/$sort/$_case.txt
@@ -33,10 +34,11 @@ do
             for i in {1..10}
             do
                 printf "TEST[%d / %d]: %s %s %s\n" $actual_test $number_of_test $sort $n $_case
-                et=$(./bin/app $sort $_case $n)
+                et=$(./bin/app $sort $_case $n $i)
                 elapsed_time=$(($elapsed_time + $et))
                 actual_test=$((actual_test + 1))
             done
+            rm temp.arr
             elapsed_time_avg=$(bc <<< "scale=0;$elapsed_time/10")
             echo $n $elapsed_time_avg
             printf "%d %.0f\n" $n $elapsed_time_avg >> benchmarks/$sort/$_case.txt  
